a7cdce440fe26b548204dee25b77080418cd2e6a
Sergey Klimov <sergey.v.klimov@gmail.com>
2018-06-01 23:06:45 -0700

    updated

--- tap_to_awesome.js
+++ tap_to_awesome.js
@@ -1,4 +1,7 @@
-var tap_to_awesome = require("./index.js");
+#!/usr/bin/env node
+
+
+var tap_to_awesome = require("tap-mochawesome-reporter");
 
 tap_to_awesome(process.stdin, function(result) {
 	console.log(JSON.stringify(result, null, 4));
